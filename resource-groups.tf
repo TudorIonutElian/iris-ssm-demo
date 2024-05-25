1f5009d6817b26c98d566fb6219ff2804e11edab
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 10:51:31 +0300

    Fix instances resourse name

--- resource-groups.tf
+++ resource-groups.tf
@@ -22,8 +22,8 @@ resource "aws_ssm_association" "ec2_development_instances_association" {
   count            = 3
   name             = aws_ssm_document.apache_document.name
   instance_id      = [
-    aws_instances.ec2_development_instances[0].id, 
-    aws_instances.ec2_development_instances[1].id, 
-    aws_instances.ec2_development_instances[2].id]
+    aws_instances.ssm_instances[0].id, 
+    aws_instances.ssm_instances[1].id, 
+    aws_instances.ssm_instances[2].id]
   schedule_expression = "rate(30 minutes)"
 }
