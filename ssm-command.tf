6af62ee9ba78d6fd06fb90fb57482c82c968bce3
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 11:18:29 +0300

    Add ssm command

---
+++ ssm-command.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "last_resource" {
+  depends_on = [
+    aws_instance.ssm_instances, aws_resourcegroups_group.development_ec2_resources
+  ]
+
+  provisioner "local-exec" {
+    command = "aws ssm send-command --document-name \"apache-server\" --targets Key=resource-groups:Name,Values=Development-EC2-Resources --region eu-central-1"
+  }
+}
