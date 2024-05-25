1ad8a31bb23f9866a646308cb6069eb2f68e554f
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 20:19:46 +0300

    New rules for print

--- ssm-command.tf
+++ ssm-command.tf
@@ -6,18 +6,19 @@ resource "null_resource" "command_execution" {
 
     provisioner "local-exec" {
         command = <<EOT
-                touch instance_ids_${BUILD_NUMBER}.txt
+                touch instance_ids_${var.BUILD_NUMBER}.txt
 
                 aws resource-groups list-group-resources \
                     --group-name "Development-EC2-Resources" \
                     --query 'ResourceIdentifiers[?ResourceType==`AWS::EC2::Instance`].ResourceArn' \
-                    --output json | jq -r '.[] | split("/") | .[-1]' > instance_ids_${BUILD_NUMBER}.txt
+                    --output json | jq -r '.[] | split("/") | .[-1]' > instance_ids_${var.BUILD_NUMBER}.txt
                 
-                mapfile -t instance_ids < instance_ids_${BUILD_NUMBER}.txt
+                mapfile -t instance_ids < instance_ids_${var.BUILD_NUMBER}.txt
                 
                 INSTANCE_IDS=$(IFS=','; echo "${instance_ids[*]}")
 
-                cat instance_ids_${BUILD_NUMBER}.txt
+                cat instance_ids_${var.BUILD_NUMBER}.txt
+                cat $INSTANCE_IDS
         EOT
     }
 
