d7d20ea422692a6ff7c55f18c455ca48c1b90979
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 21:56:01 +0300

    Try with new targets variable

--- scripts/ssm-apache-document-command.sh
+++ scripts/ssm-apache-document-command.sh
@@ -17,10 +17,12 @@ output=$(printf ", \"%s\"" "${ids[@]}")
 # Remove the leading comma and space
 output=${output:2}
 
+targets='[{"Key":"InstanceIds","Values":['$output']}]'
+
 # Print the instance IDs in the format required by the SSM document
 aws ssm send-command --document-name "apache_document" \
                 --document-version "1" \
-                --targets '[{"Key":"InstanceIds","Values":['$output']}]' \
+                --targets $targets \
                 --parameters '{"Message":["Welcome to the Apache Server for IRIS Demo!"]}' \
                 --timeout-seconds 600 --max-concurrency "50" \
                 --max-errors "0" --region eu-central-1
