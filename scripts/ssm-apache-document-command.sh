a338c0bb22562862f058fa00ab65fdcd9c590bc3
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 21:31:46 +0300

    Add the new SSM logic and separate jobs

--- scripts/ssm-apache-document-command.sh
+++ scripts/ssm-apache-document-command.sh
@@ -4,3 +4,5 @@ aws resource-groups list-group-resources \
     --group-name "Development-EC2-Resources" \
     --query 'ResourceIdentifiers[?ResourceType==`AWS::EC2::Instance`].ResourceArn' \
     --output json | jq -r '.[] | split("/") | .[-1]' > instance_ids_${BUILD_NUMBER}.txt
+
+cat instance_ids_${BUILD_NUMBER}.txt
