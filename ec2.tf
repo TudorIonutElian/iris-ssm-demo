4953a90b18ceb16179a31e67460b0c1e400dc42a
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 20:53:08 +0300

    Add iam role

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "ssm_instances" {
   instance_type = var.ssm_instance_type
   key_name      = aws_key_pair.ssm_key.key_name
   count         = var.ssm_instance_count
+  aws_iam_role  = aws_iam_role.ec2_ssm_role.name
 
   tags = {
     Name        = "SSM Instance"
---
+++ iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "EC2RoleWithSSMAccess"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
