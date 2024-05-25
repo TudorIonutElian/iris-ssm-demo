9c9fe4deed8ca7f0ef58663a0fc81f11b63174c4
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 21:23:37 +0300

    Add nee step

---
+++ scripts/ssm-apache-document-command.tf
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws resource-groups list-group-resources \
+    --group-name "Development-EC2-Resources" \
+    --query 'ResourceIdentifiers[?ResourceType==`AWS::EC2::Instance`].ResourceArn' \
+    --output json | jq -r '.[] | split("/") | .[-1]' > instance_ids_${BUILD_NUMBER}.txt
