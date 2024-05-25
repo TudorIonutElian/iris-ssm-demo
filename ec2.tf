8708af1aec92fa73bc4e74e430b29e56f2fcc269
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 20:59:21 +0300

    Fix role with profile

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ssm_instances" {
   instance_type        = var.ssm_instance_type
   key_name             = aws_key_pair.ssm_key.key_name
   count                = var.ssm_instance_count
-  iam_instance_profile = aws_iam_role.ec2_ssm_role.name
+  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
 
   tags = {
     Name        = "SSM Instance"
--- iam.tf
+++ iam.tf
@@ -21,3 +21,8 @@ resource "aws_iam_role_policy_attachment" "ec2_ssm_policy" {
   role       = aws_iam_role.ec2_ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "ec2_ssm_profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
