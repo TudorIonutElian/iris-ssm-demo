20ab8492f5d3450b4320947c2790ba9a116d93c8
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 21:40:46 +0300

    New try with bash

--- scripts/ssm-apache-document-command.sh
+++ scripts/ssm-apache-document-command.sh
@@ -5,4 +5,17 @@ aws resource-groups list-group-resources \
     --query 'ResourceIdentifiers[?ResourceType==`AWS::EC2::Instance`].ResourceArn' \
     --output json | jq -r '.[] | split("/") | .[-1]' > instance_ids_${BUILD_NUMBER}.txt
 
+# Print the instance IDs
 cat instance_ids_${BUILD_NUMBER}.txt
+
+# Print the instance IDs in the format required by the SSM document
+mapfile -t ids < instance_ids_${BUILD_NUMBER}.txt
+
+# Convert the array to a string
+output=$(printf ", \"%s\"" "${ids[@]}")
+
+# Remove the leading comma and space
+output=${output:2}
+
+# Print the instance IDs in the format required by the SSM document
+echo "\"${output}\""
