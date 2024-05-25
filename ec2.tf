40e6f2df9ad2a8763e87bc98a0bd6654e00c1e5f
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 20:54:20 +0300

    Add the role

--- ec2.tf
+++ ec2.tf
@@ -2,11 +2,11 @@
   # Create Ec2 Instance called iris_tf_demo_ec2_instance
 **********************************************************/
 resource "aws_instance" "ssm_instances" {
-  ami           = data.aws_ami.iris_ssm_ami_filter.id
-  instance_type = var.ssm_instance_type
-  key_name      = aws_key_pair.ssm_key.key_name
-  count         = var.ssm_instance_count
-  aws_iam_role  = aws_iam_role.ec2_ssm_role.name
+  ami                  = data.aws_ami.iris_ssm_ami_filter.id
+  instance_type        = var.ssm_instance_type
+  key_name             = aws_key_pair.ssm_key.key_name
+  count                = var.ssm_instance_count
+  iam_instance_profile = aws_iam_role.ec2_ssm_role.name
 
   tags = {
     Name        = "SSM Instance"
