1e40e4c2ed4bb03e38a917de537b13b7a6bb2934
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-25 10:00:50 +0300

    Add ami in scope

--- variables.tf
+++ variables.tf
@@ -1,11 +1,29 @@
-variable ssm_instance_type {
+variable "ssm_instance_type" {
   type        = string
   default     = 5
   description = "SSM instance type"
 }
 
-variable ssm_instance_count {
+variable "ssm_instance_count" {
   type        = number
   default     = 3
   description = "SSM instance count"
+}
+
+variable "startsWith" {
+  description = "The prefix of the AMI name"
+  type = string
+  default = "al2023-ami"
+}
+
+variable "endsWith" {
+  description = "The suffix of the AMI name"
+  type = string
+  default = "x86_64"
+}
+
+variable "architecture" {
+  description = "The architecture of the AMI"
+  type = string
+  default = "x86_64"
 }
